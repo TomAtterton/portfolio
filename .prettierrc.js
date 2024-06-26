e84de1ecb7561ad67b748d6e1c9c8fd96c7f5253
Tom Atterton <tpatterton@gmail.com>
2024-06-26 11:18:01 +0200

    Add projects worked on
    - add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  singleQuote: true,
+  arrowParens: 'always',
+};
