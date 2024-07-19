4c70fa9b6f3fa281975e0f43f4eac956a28a4b1f
Tom Atterton <tpatterton@gmail.com>
2024-07-19 16:12:43 +0200

    Support privacy policy for app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/recipe-vault/privacy-policy',
+        destination: '/recipe-vault/privacy-policy.js',
+      },
+    ];
+  },
   experimental: {
     typedRoutes: true,
   },
